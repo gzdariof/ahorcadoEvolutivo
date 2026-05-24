5b441f894d2f4df4c5241d86212e6a211bbf46df
Dario F. Gomez Z. <gz.dariof@gmail.com>
2026-05-23 19:26:55 -0500

    Inicio de práctica

---
+++ ahorcado.js
@@ -0,0 +1,33 @@
+'use strict';
+ 
+let palabra = "AHORCADO";
+let palabraOculta = "";
+let intentos = 6;
+
+for (let i = 0; i < palabra.length; i++) {
+    palabraOculta += "_";
+}
+
+console.log(palabraOculta);
+while (intentos > 0) {
+    let letraDigitada = prompt("Ingrese una letra:").toUpperCase();
+    if (palabra.includes(letraDigitada)) {
+        let nuevaPalabraOculta = "";
+        for (let i = 0; i < palabra.length; i++) {
+            if (palabra[i] === letraDigitada) {
+                nuevaPalabraOculta += letraDigitada;
+            } else {
+                nuevaPalabraOculta += palabraOculta[i];
+            }
+        }
+        palabraOculta = nuevaPalabraOculta;
+        alert("¡Correcto!: " + palabraOculta);
+        if (palabraOculta === palabra) {
+            alert("¡Felicidades! Has ganado. La palabra era: " + palabra);
+            break;
+        }
+    } else {
+        intentos--;
+        alert("¡Incorrecto! Intentos restantes: " + intentos);
+    }
+}
